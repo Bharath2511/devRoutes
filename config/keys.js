be23fa7b2d9a91e06170722e8a40850403d2881a
V S Bharath <bharathcse5t6@gmail.com>
2018-10-24 23:05:54 +0530

    retrieving json webtokens

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://bharath:b%408686779678@ds163681.mlab.com:63681/dev-connector"
+    "mongodb://bharath:b%408686779678@ds163681.mlab.com:63681/dev-connector",
+  secretOrKey: "secret"
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-
+const keys = require("../../config/keys");
 //loading models
 const User = require("../../models/User");
 
@@ -63,7 +63,24 @@ router.post("/login", (req, res) => {
     //check password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: "success" });
+        //user matched
+        const payload = {
+          id: user.id,
+          name: user.name,
+          avatar: user.avatar
+        }; //create jwt payload
+        //sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
       } else {
         return res.status(400).json({ password: "password incorrect" });
       }

8c8be00b0de4f6d59475cc711e741789c6ef9905
V S Bharath <bharathcse5t6@gmail.com>
2018-10-24 20:12:44 +0530

    register route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const User = require("../../models/User");
 
 //GET api/users/test
 //tests users route
@@ -6,6 +7,24 @@ const router = require("express").Router();
 
 router.get("/test", (req, res) => {
   res.json({ msg: "users works" });
+});
+
+//GET api/users/register
+//register users
+//public access
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "email already exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+    }
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+//bp middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("hello");

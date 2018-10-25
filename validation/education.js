9631d341ec043d51b57dbee1e4f2004ce86e45d8
V S Bharath <bharathcse5t6@gmail.com>
2018-10-25 20:48:34 +0530

    education routes and validations are done

--- validation/education.js
+++ validation/education.js
@@ -5,17 +5,23 @@ module.exports = function validateExperienceInput(data) {
   let errors = {};
   //if its not empty then then let it be. If its empty then
   //it is an empty string
-  data.title = !isEmpty(data.title) ? data.title : "";
-  data.company = !isEmpty(data.company) ? data.company : "";
+  data.school = !isEmpty(data.school) ? data.school : "";
+  data.degree = !isEmpty(data.degree) ? data.degree : "";
+  data.fieldofstudy = !isEmpty(data.fieldofstudy) ? data.fieldofstudy : "";
   data.from = !isEmpty(data.from) ? data.from : "";
 
-  if (validator.isEmpty(data.title)) {
-    errors.title = "Job title field is required";
+  if (validator.isEmpty(data.school)) {
+    errors.school = "School field is required";
   }
 
-  if (validator.isEmpty(data.company)) {
-    errors.company = "company field is required";
+  if (validator.isEmpty(data.degree)) {
+    errors.degree = "Degree field is required";
   }
+
+  if (validator.isEmpty(data.fieldofstudy)) {
+    errors.fieldofstudy = "Field of study field is required";
+  }
+
   if (validator.isEmpty(data.from)) {
     errors.from = "from Date field is required";
   }

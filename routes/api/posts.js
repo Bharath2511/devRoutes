24bb873a1dd86714def7a51a2e242f76d4daff85
V S Bharath <bharathcse5t6@gmail.com>
2018-10-26 00:09:44 +0530

    like and unlike

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -21,7 +21,13 @@ router.get("/test", (req, res) => {
 router.get("/", (req, res) => {
   Post.find()
     .sort({ date: -1 })
-    .then(posts => res.json(posts))
+    .then(posts => {
+      if (posts.length < 1 || null) {
+        res.status(404).json({ noposts: "no posts found" });
+      } else {
+        res.json(posts);
+      }
+    })
     .catch(err => res.status(404).json({ nopostsfound: "no posts found" }));
 });
 
@@ -79,6 +85,67 @@ router.delete(
           }
           //delete
           post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
+  }
+);
+//id is post id
+//post api/posts/like/:id
+//like posts
+//private access
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: "user already liked this post" });
+          }
+          //add the user id to likes array
+          post.likes.unshift({ user: req.user.id });
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
+  }
+);
+
+//id is post id
+//post api/posts/unlike/:id
+//unlike posts
+//private access
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ notliked: "you have not yet liked this post" });
+          }
+          //get remove index
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+          //splice
+          post.likes.splice(removeIndex, 1);
+          //save
+          post.save().then(post => res.json(post));
         })
         .catch(err => res.status(404).json({ postnotfound: "No post found" }));
     });

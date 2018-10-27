665d625fa926ef7ffd735d78ed3e5a8006504ad1
V S Bharath <bharathcse5t6@gmail.com>
2018-10-27 13:08:21 +0530

    required router created login register routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+//browser route to go back and forth route to create individual route
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -7,11 +9,13 @@ import Landing from "./components/layout/Landing";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }

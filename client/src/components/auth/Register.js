8dc995c89be16d7801335c8eca8e1a3252257cc8
V S Bharath <bharathcse5t6@gmail.com>
2018-10-27 19:33:10 +0530

    integrating register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Register extends Component {
   constructor() {
@@ -28,7 +29,13 @@ class Register extends Component {
       password: this.state.password,
       password2: this.state.password2
     };
-    console.log(newUser);
+    //we are using it in onsubmit because we need to save credentials to
+    //db on submitting
+    //axios.post 1)route 2)data that needed to be posted
+    axios
+      .post("/api/users/register", newUser)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data));
   }
   render() {
     return (

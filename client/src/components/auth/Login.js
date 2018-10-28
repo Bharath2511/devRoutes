685c3f1ba6d8a76e6f13d8a5079c17890be6a2ee
V S Bharath <bharathcse5t6@gmail.com>
2018-10-28 20:58:20 +0530

    text-field-group

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import propTypes from "prop-types";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import classnames from "classnames";
 import { loginUser } from "../../actions/authActions";
 import TextFieldGroup from "../common/TextFieldGroup";
 
@@ -11,11 +10,9 @@ class Login extends Component {
     this.state = {
       email: "",
       password: "",
-
       errors: {}
     };
 
-    //we are binding this of onchange
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
@@ -30,6 +27,7 @@ class Login extends Component {
     if (nextProps.auth.isAuthenticated) {
       this.props.history.push("/dashboard");
     }
+
     if (nextProps.errors) {
       this.setState({ errors: nextProps.errors });
     }
@@ -37,74 +35,51 @@ class Login extends Component {
 
   onSubmit(e) {
     e.preventDefault();
+
     const userData = {
       email: this.state.email,
       password: this.state.password
     };
-    //we have to call the action
+
     this.props.loginUser(userData);
   }
+
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
 
   render() {
     const { errors } = this.state;
+
     return (
-      <div>
-        <div className="login">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-8 m-auto">
-                <h1 className="display-4 text-center">Log In</h1>
-                <p className="lead text-center">
-                  Sign in to your DevConnector account
-                </p>
-                <form onSubmit={this.onSubmit}>
-                  <TextFieldGroup
-                    placeholder="Email Address"
-                    name="email"
-                    type="email"
-                    value={this.state.email}
-                    onChange={this.onChange}
-                    error={errors.email}
-                  />
-                  <div className="form-group">
-                    <input
-                      type="email"
-                      className={classnames("form-control form-control-lg", {
-                        "is-invalid": errors.email
-                      })}
-                      placeholder="Email Address"
-                      name="email"
-                      value={this.state.email}
-                      onChange={this.onChange}
-                    />
-                    {errors.email && (
-                      <div className="invalid-feedback">{errors.email}</div>
-                    )}
-                  </div>
-                  <div className="form-group">
-                    <input
-                      type="password"
-                      className={classnames("form-control form-control-lg", {
-                        "is-invalid": errors.password
-                      })}
-                      placeholder="Password"
-                      name="password"
-                      value={this.state.password}
-                      onChange={this.onChange}
-                    />
-                    {errors.password && (
-                      <div className="invalid-feedback">{errors.password}</div>
-                    )}
-                  </div>
-                  <input
-                    type="submit"
-                    className="btn btn-info btn-block mt-4"
-                  />
-                </form>
-              </div>
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
+                Sign in to your DevConnector account
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="Email Address"
+                  name="email"
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  error={errors.email}
+                />
+
+                <TextFieldGroup
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  error={errors.password}
+                />
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
             </div>
           </div>
         </div>
@@ -114,11 +89,10 @@ class Login extends Component {
 }
 
 Login.propTypes = {
-  loginUser: propTypes.func.isRequired,
-  auth: propTypes.object.isRequired,
-  errors: propTypes.object.isRequired
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
-
 //component will recieve it from reducer and maps it to the state
 const mapStateToProps = state => ({
   auth: state.auth,
--- client/src/components/common/TextFieldGroup.js
+++ client/src/components/common/TextFieldGroup.js
@@ -1,6 +1,6 @@
 import React from "react";
 import classnames from "classnames";
-import propTypes from "prop-types";
+import PropTypes from "prop-types";
 
 const TextFieldGroup = ({
   name,
@@ -16,7 +16,7 @@ const TextFieldGroup = ({
   return (
     <div className="form-group">
       <input
-        type="email"
+        type={type}
         className={classnames("form-control form-control-lg", {
           "is-invalid": error
         })}
@@ -26,22 +26,21 @@ const TextFieldGroup = ({
         onChange={onChange}
         disabled={disabled}
       />
-
-      {info && <small className="form-text text-muted" />}
+      {info && <small className="form-text text-muted">{info}</small>}
       {error && <div className="invalid-feedback">{error}</div>}
     </div>
   );
 };
 
 TextFieldGroup.propTypes = {
-  name: propTypes.string.isRequired,
-  placeholder: propTypes.string,
-  value: propTypes.string.isRequired,
-  info: propTypes.string,
-  error: propTypes.string,
-  type: propTypes.string.isRequired,
-  onChange: propTypes.func.isRequired,
-  disabled: propTypes.string
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  info: PropTypes.string,
+  error: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  disabled: PropTypes.string
 };
 
 TextFieldGroup.defaultProps = {

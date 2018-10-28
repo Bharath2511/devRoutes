08abbd55647003a94267de39c204e1d33e595818
V S Bharath <bharathcse5t6@gmail.com>
2018-10-28 17:57:01 +0530

    token for login

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
 //because we want to register a user through actions
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
 import axios from "axios";
 
 //register USer
@@ -14,4 +16,38 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+//Login - get user token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      //save to local storage
+      const { token } = res.data;
+      //set token to local storage
+      localStorage.setItem("jwtToken", token);
+      //set token to auth header
+      setAuthToken(token);
+      //this token contains user data
+      //decode token to get user data
+      const decoded = jwt_decode(token);
+      //set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//set logged in user
+export const setCurrentUser = decoded => {
+  //dispatch to reducer
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,14 @@
+//setting a default header for axios
+import axios from "axios";
+//if we are logged in its always gonna attach authorix=zation error
+const setAuthToken = token => {
+  if (token) {
+    //apply to every request
+    axios.defaults.headers.common["Authorization"] = token;
+  } else {
+    //delete the auth header if token isn't there
+    delete axios.defaults.headers.commom["Authorization"];
+  }
+};
+
+export default setAuthToken;

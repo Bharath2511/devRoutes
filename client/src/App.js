108267a202e5f1f387583a9dce8bc9fb4e1eab11
V S Bharath <bharathcse5t6@gmail.com>
2018-10-29 10:33:39 +0530

    profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 //browser route to go back and forth route to create individual route
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
@@ -8,6 +8,8 @@ import { clearCurrentProfile } from "./actions/profileActions";
 //provider is react component which holds the react state all the data
 import { Provider } from "react-redux";
 import store from "./store";
+
+import PrivateRoute from "./components/common/PrivateRoute";
 
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
@@ -48,7 +50,9 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/Login" component={Login} />
-              <Route exact path="/dashboard" component={Dashboard} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
             </div>
             <Footer />
           </div>
---
+++ client/src/components/create-profile/CreateProfile.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+class CreateProfile extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default connect(null)(CreateProfile);

e184260059738980fcfe17782b28613ab98828ec
V S Bharath <bharathcse5t6@gmail.com>
2018-11-05 20:05:20 +0530

    footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,9 +1,10 @@
 import React from "react";
+//footer is awesome
 //footer
 
 export default function Footer() {
   return (
-    <footer className="bg-dark text-white mt-5 p-4 text-center fixed-bottom">
+    <footer className="bg-dark text-white mt-5 p-2 text-center fixed-bottom">
       Copyright &copy; {new Date().getFullYear()} DevRoutes
     </footer>
   );
